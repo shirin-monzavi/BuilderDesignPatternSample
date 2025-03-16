36179e20cc0ffe194c00fff1066c1d67cb98d2e9
monzavi.shirin <monzavi.shirin@GPA001P2102485>
2025-03-16 09:59:26 +0330

    clean code

--- BuilderDesignPatternSample/Program.cs
+++ BuilderDesignPatternSample/Program.cs
@@ -1,12 +1,11 @@
 ﻿using BuilderDesignPatternSample.Builders.Contracts;
 using BuilderDesignPatternSample.Builders.Implements;
 using BuilderDesignPatternSample.Directors;
-using BuilderDesignPatternSample.Products;
 
 ComputerShop shop = new();
 ComputerBuilder builder = new GamingComputerBuilder();
 
 shop.AssembleComputer(builder);
 
-Computer computer = builder.GetComputer();
+var computer = builder.GetComputer();
 computer.DisplaySpecifications();
